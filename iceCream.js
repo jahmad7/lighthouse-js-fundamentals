784f62edd9de60ef1ea5beea176dcee5a19bccf1
warriorjj <jahmad095@gmail.com>
2019-03-18 02:58:19 +0000

    ice cream assignment completed

---
+++ iceCream.js
@@ -0,0 +1,16 @@
+var iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+//add root beer 
+iceCreamFlavours.push("root beer");
+console.log(iceCreamFlavours);
+
+//choclate
+console.log(iceCreamFlavours[0]);
+
+
+//last item
+console.log(iceCreamFlavours[iceCreamFlavours.length - 1]);
+
+//number of items
+console.log(iceCreamFlavours.length);
